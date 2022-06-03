d1ea777baf74177016ad0397c8a485c310095f5e
Max Girteit <icufishmg@gmail.com>
2022-06-03 12:42:12 -0600

    layout added to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import Layout from '../components/Layout'
 
 const About = () => {
   return (
-    <>
+    <Layout>
 
       <div>About</div>
-    </>
+    </Layout>
   )
 }
 
