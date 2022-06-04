648920241eeed7241aef5e49a465abcea4f97859
Max Girteit <icufishmg@gmail.com>
2022-06-04 14:06:47 -0600

    navbar button and logo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,39 @@
-import React from 'react'
-import {Link} from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
+import { FiAlignJustify } from "react-icons/fi"
+
+import glasses from "../assets/images/nouns_glasses.png"
 
 const Navbar = () => {
   return (
-    <nav>
-<ul>
-    <li>
-<Link to='/'>Home</Link>
-    </li>
-    <li>
-<Link to='/about'>About</Link>
-    </li>
-    <li>
-<Link to='/contact'>Contact</Link>
-    </li>
-    <li>
-<Link to='/merch'>Merch</Link>
-    </li>
-    <li>
-<Link to='/tags'>tags</Link>
-    </li>
-</ul>
-
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={glasses} alt="Nouns Glasses" />
+          </Link>
+          <button className="nav-btn">
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to="/" className="nav-link" activeClassName="active-link">
+            home
+          </Link>
+          <Link to="/merch" className="nav-link" activeClassName="active-link">
+            merch
+          </Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">
+            tags
+          </Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">
+            about
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">contact</Link>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
