e47d8c45808442adff57f4da15ea5c3a612d86c1
Max Girteit <icufishmg@gmail.com>
2022-06-04 12:45:22 -0600

    footer content added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className='page-footer'>
+      <p>CC0 {new Date().getFullYear()} <span>Nouns Merch</span></p>
+    </footer>
   )
 }
 
